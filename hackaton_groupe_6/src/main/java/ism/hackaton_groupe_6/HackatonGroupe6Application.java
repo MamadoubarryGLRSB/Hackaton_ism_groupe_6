e906c94f542a28bdf8b497684880444d8a702cfc
MamadoubarryGLRSB <92933600+MamadoubarryGLRSB@users.noreply.github.com>
2023-05-27 10:01:28 +0000

    Add files via upload

---
+++ hackaton_groupe_6/src/main/java/ism/hackaton_groupe_6/HackatonGroupe6Application.java
@@ -0,0 +1,13 @@
+package ism.hackaton_groupe_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonGroupe6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonGroupe6Application.class, args);
+	}
+
+}
